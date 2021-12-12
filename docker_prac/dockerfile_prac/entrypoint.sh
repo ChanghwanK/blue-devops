a792b7ffb21914a8a435882e91d391874ae036fe
DevBloo <devchanghwan@gmail.com>
2021-12-12 17:36:53 +0900

    init: initial commit

---
+++ docker_prac/dockerfile_prac/entrypoint.sh
@@ -0,0 +1,3 @@
+# bin/bash는 ENTRYPOINT로 대체했음
+echo $1 $2
+apachectl -DFOREGROUND
---
+++ shell_script_prac/shell_script_prac.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# echo test
+echo "start shell script test"
+
+
+# Variable
+var1='test'
+var2=12
+
+echo 'variable test'
+echo $var1
+echo $var2
+
+echo '${} variable test'
+echo ${var1}
+echo ${var2}
+
